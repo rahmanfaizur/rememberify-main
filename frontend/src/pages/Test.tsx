b956f397994babd24387135f19422a595cf3189f
rahmanfaizur <rahman.faizur.fr@gmail.com>
2024-12-27 20:16:27 +0530

    minor

--- frontend/src/pages/Test.tsx
+++ frontend/src/pages/Test.tsx
@@ -1,18 +1,7 @@
-import { Button } from "../components/ui/Button";
-
 export default function Test() {
   return (
     <div className="p-4">
-      <Button
-        variant="primary"
-        size="md"
-        text="Click Me"
-        padding="one"
-        changeTextOnClick={true}
-        newText="Signin In!"
-        changeColorOnClick={true}
-        newVariant="lightBlue"
-      />
+      Heyo!
     </div>
   );
 }
