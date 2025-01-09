2bdb7a453c5219fe6b42564bff865ea89fed3cf1
rahmanfaizur <rahman.faizur.fr@gmail.com>
2025-01-09 19:02:38 +0530

    Removed test

--- frontend/src/pages/Test.tsx
+++ frontend/src/pages/Test.tsx
@@ -2,19 +2,19 @@ import { useState } from "react";
 import { Button } from "../components/ui/Button";
 
 export default function Test() {
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
-  const handleClick = () => {
-      setLoading(true); // Set loading to true
-      setTimeout(() => {
-          setLoading(false); // Reset loading after 2 seconds
-          alert("Button Clicked!"); // Simulate some action
-      }, 2000); // Simulate a delay (e.g., API call)
-  };
+  // const handleClick = () => {
+  //     setLoading(true); // Set loading to true
+  //     setTimeout(() => {
+  //         setLoading(false); // Reset loading after 2 seconds
+  //         alert("Button Clicked!"); // Simulate some action
+  //     }, 2000); // Simulate a delay (e.g., API call)
+  // };
 
   return (
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-          <h1 className="text-2xl font-bold mb-4">Test the Button Component</h1>
+          {/* <h1 className="text-2xl font-bold mb-4">Test the Button Component</h1>
           <Button
         variant="primary"
         size="md"
@@ -22,7 +22,8 @@ export default function Test() {
         onClick={handleClick}
         loading={loading}
         fullWidth={false} // Set to true if you want the button to stretch
-        padding={"one"}          />
+        padding={"one"}          /> */}
+        Open Endpoint!
       </div>
   );
 }
