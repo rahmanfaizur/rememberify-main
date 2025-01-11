aa856860e2245a669b0c333f1708b2fe6622ab83
rahmanfaizur <rahman.faizur.fr@gmail.com>
2025-01-11 20:32:21 +0530

    Added changes to the error page

--- frontend/src/icons/index.ts
+++ frontend/src/icons/index.ts
@@ -1,10 +1,12 @@
 export interface IconProps {
-    size: "sm" | "md" | "lg"
+    size: "sm" | "md" | "lg" | "xl" | "xxl" | "errorPage"
 }
 
 export const iconSizeVariants = {
     "sm": "size-2",
     "md": "size-4",
     "lg": "size-6",
-    "xl": "size-8"
+    "xl": "size-8",
+    "xxl": "size-10",
+    "errorPage": "size-60"
 }
--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,7 +1,16 @@
+import { ThinkIcon } from "../icons/ThinkIcon";
+
 export function ErrorPage() {
     return (
-        <div>
-        Error Page!
+        <div className="bg-gray-900 text-white h-screen grid grid-cols-2 gap-5 items-center">
+        <div className="pl-20" >
+        <div className="font-bold text-3xl">Error 404!</div>
+        <li className="pl-4">
+            <li>fjnds</li>
+            <li>fjnds</li>
+        </li>
+        </div>
+        <ThinkIcon size="errorPage"></ThinkIcon>
         </div>
     )
 }
