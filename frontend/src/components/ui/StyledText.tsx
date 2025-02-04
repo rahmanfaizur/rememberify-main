7aa412f6496d7d7d705f096f82f5f921fc8a2ee1
rahmanfaizur <rahman.faizur.fr@gmail.com>
2025-02-04 18:56:12 +0530

    Added formating to the StyledComponent component

--- frontend/src/components/ui/StyledText.tsx
+++ frontend/src/components/ui/StyledText.tsx
@@ -3,25 +3,29 @@ import './styles.css';
 const GetStuffDone = () => {
   return (
     <div className="contentContainer text-center font-mono">
-      <div className="getStuffDone">
-        {/* "Get Stuff" heading with rotation */}
+      <div 
+        className="getStuffDone 
+          pt-[100px] sm:pt-[120px] md:pt-0" 
+        // Adds padding on smaller screens
+      >
+        {/* "Get Stuff" heading with 15-degree rotation and boldness */}
         <h1
           className={`
-            gs block max-w-full h-auto
+            gs block max-w-full h-auto font-extrabold
             text-[60px] sm:text-[80px] md:text-[100px] lg:text-[120px]
             leading-[1] sm:leading-[0.9] md:leading-[0.8] lg:leading-[0.9]
-            transform -rotate-6 origin-top-left
+            transform -rotate-[15deg] origin-top-left
           `}
         >
           Get Stuff
         </h1>
-        {/* "DONE!" heading with rotation */}
+        {/* "DONE!" heading with 15-degree rotation and boldness */}
         <h1
           className={`
-            gsd
+            gsd font-extrabold
             text-[80px] sm:text-[120px] md:text-[150px] lg:text-[180px]
             leading-[0.85] sm:leading-[1] md:leading-[1.1] lg:leading-[1.1]
-            transform -rotate-6 origin-top-left
+            transform -rotate-[15deg] origin-top-left
           `}
         >
           DONE!
